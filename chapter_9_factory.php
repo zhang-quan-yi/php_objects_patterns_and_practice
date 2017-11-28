53dd7b54acc9cdda78e0c8f39ab289202f17c3f5
zqy <zh.hailei@gmail.com>
2017-11-28 10:21:41 +0800

    添加静态方法和静态属性来间接实例化对象

--- chapter_9_factory.php
+++ chapter_9_factory.php
@@ -1,9 +1,19 @@
 <?php
-// 单例模式
+// 9.2 单例模式
+// 添加静态方法和静态属性来间接实例化对象
+
 class Preferences {
+    private static $instance;
     private $props = [];
 
     private function __construct(){}
+
+    public static function getInstance(){
+        if(empty(self::$instance)){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
 
     public function setProperty($key,$value){
         $this->props[$key] = $value;
