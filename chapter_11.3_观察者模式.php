1b988ef649b20d67316f4b07b4292601a48a165e
zqy <zh.hailei@gmail.com>
2018-01-02 09:19:07 +0800

    11.3 观察者模式 增加需求

--- chapter_11.3_观察者模式.php
+++ chapter_11.3_观察者模式.php
@@ -2,6 +2,8 @@
 // 正交性：
 // 程序的目标应该是创建在改动和转移时对其他组件影响最小对组件。
 
+// 为 handleLogin函数添加新的功能：记录访问者ip、登入失败发送邮件等等。。。
+
 class Login{
     const LOGIN_USER_UNKNOWN = 1;
     const LOGIN_WRONG_PASS = 2;
@@ -24,6 +26,13 @@ class Login{
                 $ret = false;
                 break;
         }
+        // Logger::logIP($user,$ip,$this->getStatus());
+
+        // if(!$ret){
+        //  Notifier::mailWarning($user,$ip,$this->getStatus());
+        //}
+
+        // return $ret;
     }
 
     private function setStatus($status,$user,$ip){
@@ -35,3 +44,4 @@ class Login{
     }
 }
 
+// 如果像这样直接在代码中加入功能来满足需求，会破坏设计，Login类很快会紧紧嵌入到这个特殊的系统中；
